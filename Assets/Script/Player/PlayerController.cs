ac93df6c6cc9ab60e1c0bfe62c9219d0c6d2c099
JunHyung777 <jameskjh7531@gmail.com>
2024-05-19 20:59:09 +0900

    feat: 1단 점프 & 2단 점프 구현 완료
    1. Update 메소드 내에서 "Jump"입력을 감지해 점프하도록 구현
    2. jumpCount 변수로 현재 점프 횟수를 계산하고 maxJumpCount로 점프 한계 횟수를 지정 (현재 2회)
    3. Ground 태그를 가진 오브젝트와 충돌하면 jumpCount를 0으로 초기화
    4. 테스트 Ground 오브젝트의 길이를 늘렸고 "Ground"태그 지정, PlayerData.json에서 speed 값 5로 변경함

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -3,9 +3,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private PlayerData _playerData = null;
-    
+    private Rigidbody2D _rigidbody2D;
+    public float jumpForce = 10.0f; // 점프 힘 - 점프 힘과 RigidBody 2D의 Gravity Scale 값을 조정하여 점프 조정 가능
+    private int jumpCount = 0;      // 현재 점프 횟수
+    private int maxJumpCount = 2;   // 최대 점프 횟수 (더블 점프)
+
     private void Start()
     {
         _playerData = DataManager.Instance.LoadData();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumpCount)
+        {
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpForce);
+            jumpCount++;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCount = 0; // 땅에 닿으면 점프 카운트 초기화
+        }
     }
 }
