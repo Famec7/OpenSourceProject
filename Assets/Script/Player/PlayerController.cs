da2e92ba89d11715b2d767c8b8c4e667e36f7c46
JunHyeong777 <jameskjh7531@gmail.com>
2024-05-20 13:05:27 +0900

    refactor: PlayerController 리팩토링
    1. GetComponent Start에서 Awake에서 호출하도록 변경
    2. Player의 상태를 bool에서 enum으로 관리하도록 변경
    3. 주석 추가

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -4,68 +4,98 @@ public class PlayerController : MonoBehaviour
 {
     private PlayerData _playerData = null;
     private Animator _animator;
-
     private Rigidbody2D _rigidbody2D;
-    public float jumpForce = 10.0f; // 점프 힘 - 점프 힘과 RigidBody 2D의 Gravity Scale 값을 조정하여 점프 조정 가능
+    private BoxCollider2D _collider;
+
+    // 점프 관련 변수
+    public float jumpForce = 10.0f;  // 점프 시 적용될 힘 - 점프 힘과 RigidBody 2D의 Gravity Scale 값을 조정하여 점프 조정 가능   
     private int _jumpCount = 0;      // 현재 점프 횟수
     private int _maxJumpCount = 2;   // 최대 점프 횟수 (더블 점프)
-    private bool _isSliding = false;
 
-    private BoxCollider2D _collider;
+    // 콜라이더 크기 및 오프셋
     private Vector2 _standingColliderSize;
     private Vector2 _standingColliderOffset;
-    public Vector2 slidingColliderSize;
-    public Vector2 slidingColliderOffset;
+    public Vector2 slidingColliderSize;     // 슬라이딩 시 콜라이더 크기
+    public Vector2 slidingColliderOffset;   // 슬라이딩 시 콜라이더 오프셋
 
-    private bool _isJump = false;
+    // 플레이어 상태를 나타내는 열거형
+    enum PlayerState 
+    {
+        Running,
+        Jumping,
+        Sliding
+    }
 
-    private void Start()
+    private PlayerState _currentState = PlayerState.Running; // 초기 상태는 Running
+
+    private void Awake()
     {
         _playerData = DataManager.Instance.LoadData();
         _animator = GetComponent<Animator>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
-
         _collider = GetComponent<BoxCollider2D>();
-        _standingColliderSize = _collider.size; // 기존 콜라이더 크기 저장
-        _standingColliderOffset = _collider.offset; // 기존 콜라이더 오프셋 저장
+    }
 
-        // 슬라이딩 콜라이더 설정 (기존 크기의 절반)
+    private void Start()
+    {
+        // 기존 콜라이더 크기 및 오프셋 저장
+        _standingColliderSize = _collider.size; 
+        _standingColliderOffset = _collider.offset; 
+
+        // 슬라이딩 시 적용될 콜라이더 및 오프셋 설정 (기존 크기의 절반)
         slidingColliderSize = new Vector2(_collider.size.x, _collider.size.y / 2);
         slidingColliderOffset = new Vector2(_collider.offset.x, _collider.offset.y - _collider.size.y / 4);
     }
 
     private void Update()
     {
+        // 점프 입력 처리
         if (Input.GetButtonDown("Jump") && _jumpCount < _maxJumpCount)
         {
-            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpForce);
-            _jumpCount++;
-            _isJump = true;
-            _animator.SetBool("Jump", true);
+            Jump();
         }
 
-        if (Input.GetKey(KeyCode.LeftShift) && !_isSliding &&!_isJump)
+        // 슬라이딩 입력 처리
+        if (Input.GetKey(KeyCode.LeftShift) && _currentState == PlayerState.Running)
         {
             StartSlide();
-            _animator.SetBool("Slide", true);
         }
-        else if (Input.GetKeyUp(KeyCode.LeftShift) && _isSliding)
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            _animator.SetBool("Slide", false);
             StopSlide();
         }
     }
 
-    private void StartSlide()
+    private void Jump()
     {
-        _isSliding = true;
-        _collider.size = slidingColliderSize;
-        _collider.offset = slidingColliderOffset;
+        // 슬라이딩 중에 점프 시 슬라이딩을 중지
+        if (_currentState == PlayerState.Sliding)
+        {
+            StopSlide();
+        }
+
+        // 점프 로직 수행
+        _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpForce);
+        _jumpCount++;
+        _currentState = PlayerState.Jumping;
+        _animator.SetBool("Jump", true); // 점프 애니메이션 활성화
     }
 
+
+    // 슬라이딩 상태로 전환
+    private void StartSlide()
+    {
+        _currentState = PlayerState.Sliding;
+        _collider.size = slidingColliderSize;
+        _collider.offset = slidingColliderOffset;
+        _animator.SetBool("Slide", true); // 슬라이딩 애니메이션 활성화
+    }
+
+    // 슬라이딩 상태 해제
     private void StopSlide()
     {
-        _isSliding = false; 
+        _animator.SetBool("Slide", false);
+        _currentState = PlayerState.Running;
         _collider.size = _standingColliderSize;
         _collider.offset = _standingColliderOffset;
     }
@@ -76,7 +106,7 @@ public class PlayerController : MonoBehaviour
         {
             _animator.SetBool("Jump", false);
             _jumpCount = 0; // 땅에 닿으면 점프 카운트 초기화
-            _isJump = false;
+            _currentState = PlayerState.Running; // 땅에 닿으면 상태를 Running으로 전환
         }
     }
 }
