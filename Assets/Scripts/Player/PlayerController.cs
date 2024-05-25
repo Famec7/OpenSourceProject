cfccad7cd68d0bfddf8d9c5faf3e6ae62d195a59
JiHwan-Oh <jiwhanoh@gmail.com>
2024-05-25 21:56:19 +0900

    feat: 장애물 충돌 및 피격 시 무적 시스템 구현 완료
    1. OnTriggerEnter2D 함수 이용하여 장애물 충돌 구현
    2. OnDamaged, OffDamaged 함수 호출하여 무적 상태 진입/탈출
    3. _isInvincible 변수로 플레이어 무적 상태 체크
    4. 무적 상태 중에는 플레이어 Sprite 반투명하게 변경
    5. invincibleTime 변수로 무적 시간을 초 단위로 조절 가능
    6. HitDetectionTest 씬에 테스트를 위한 장애물 추가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private PlayerData _playerData = null;
+    private SpriteRenderer _spriteRenderer;
     private Animator _animator;
     private Rigidbody2D _rigidbody2D;
     private BoxCollider2D _collider;
@@ -12,6 +13,10 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10.0f;  // ���� �� ����� �� - ���� ���� RigidBody 2D�� Gravity Scale ���� �����Ͽ� ���� ���� ����   
     private int _jumpCount = 0;      // ���� ���� Ƚ��
     private int _maxJumpCount = 2;   // �ִ� ���� Ƚ�� (���� ����)
+
+    // 무적 관련 변수
+    public float invincibleTime = 2f; // 무적 시간 (s)
+    private bool _isInvincible = false;
 
     // �ݶ��̴� ũ�� �� ������
     private Vector2 _standingColliderSize;
@@ -32,6 +37,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         _playerData = DataManager.Instance.LoadData();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _collider = GetComponent<BoxCollider2D>();
@@ -101,6 +107,29 @@ public class PlayerController : MonoBehaviour
         _collider.offset = _standingColliderOffset;
     }
 
+    // 장애물 충돌 시 플레이어를 무적 상태로 만드는 함수
+    private void OnDamaged()
+    {
+        // 플레이어 무적 활성화
+        _isInvincible = true;
+
+        // 플레이어 Sprite 반투명화
+        _spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        // 무적 시간이 끝나면 무적 상태 종료 함수 호출
+        Invoke("OffDamaged", invincibleTime);
+    }
+
+    // 플레이어를 무적 상태에서 벗어나게 만드는 함수
+    private void OffDamaged()
+    {
+        // 플레이어 무적 비활성화
+        _isInvincible = false;
+
+        // 플레이어 Sprite 불투명화
+        _spriteRenderer.color = new Color(1, 1, 1, 1f);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
@@ -109,11 +138,19 @@ public class PlayerController : MonoBehaviour
             _jumpCount = 0; // ���� ������ ���� ī��Ʈ �ʱ�ȭ
             _currentState = PlayerState.Running; // ���� ������ ���¸� Running���� ��ȯ
         }
+    }
 
+    private void OnTriggerEnter2D(Collider2D Other)
+    {
         // 장애물과 충돌 시
-        if (collision.gameObject.CompareTag("Obstacle"))
+        if (Other.gameObject.CompareTag("Obstacle"))
         {
-            _playerData.hp -= 1; // player의 hp 감소
+            if (_isInvincible == false) // 플레이어가 현재 무적 상태가 아니라면
+            {
+                _playerData.hp -= 1; // player의 hp 감소
+                Debug.Log(_playerData.hp);
+                OnDamaged(); // 플레이어 무적 상태 진입 함수 호출
+            }
         }
     }
 }
