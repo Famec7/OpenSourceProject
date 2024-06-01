5c21fb2e27fe40fbd0085ee46b0131c9b0edfd81
Leekb0804 <lkb8713@gmail.com>
2024-06-01 23:01:29 +0900

    refactor: 주석 추가 및 debug.log 삭제
    1. Activate 의 debug.log 삭제
    2. HpItemEffect.cs와 HpItemData.cs 에 주석 추가

--- Assets/Scripts/Item/ItemData/HpItemData.cs
+++ Assets/Scripts/Item/ItemData/HpItemData.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "HpItemData", menuName = "ItemData/HpItemData")]
-public class HpItemData : ItemDataBase
+public class HpItemData : ItemDataBase 
 {
-    public int hpAmount;
-    public int recoverySpeed;
+    public int hpAmount;                        //회복 아이템의 총 hp 회복량
+    public int recoverySpeed;                   //회복 아이템의 초당 hp 회복량
 
 }
--- Assets/Scripts/Item/ItemEffect/HpItemEffect.cs
+++ Assets/Scripts/Item/ItemEffect/HpItemEffect.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using UnityEngine;
 
-public class HpItemEffect : ItemEffectBase
+public class HpItemEffect : ItemEffectBase          
 {
-    private float _hpAmount;
-    private float _recoverySpeed;
+    private float _hpAmount;                //HpItem의 총 회복량
+    private float _recoverySpeed;           //HpItem의 초당 회복량
 
     public override void Activate(PlayerData playerData)
     {
-        Debug.Log("TestItemEffect activated!");     //debug
-
         _hpAmount = ((HpItemData)ItemData).hpAmount;
         _recoverySpeed = ((HpItemData)ItemData).recoverySpeed;
-        ((HpItemData)ItemData).duration = _hpAmount / _recoverySpeed;
+        ((HpItemData)ItemData).duration = _hpAmount / _recoverySpeed;   //HpItem의 지속 시간 설정
         StartCoroutine(IE_RecoveryHp(playerData));
     }
 
@@ -23,11 +21,11 @@ public class HpItemEffect : ItemEffectBase
 
     private IEnumerator IE_RecoveryHp(PlayerData playerData)
     {
-        float finalHp = _hpAmount + playerData.hp;
+        float finalHp = _hpAmount + playerData.hp;          //회복되고 난 후의 playerhp
 
-        while (playerData.hp < finalHp)
+        while (playerData.hp < finalHp)                     //예상 회복량까지 회복 반복
         {
-            playerData.hp += _recoverySpeed * Time.deltaTime;
+            playerData.hp += _recoverySpeed * Time.deltaTime;       //playerhp를 고르게 회복
             yield return null;
         }
     }
