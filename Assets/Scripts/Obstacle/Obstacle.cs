e107a0b2919d09c2525ceb6d7f4067fcc4d3c8a9
Famec7 <power4417@gmail.com>
2024-06-02 14:37:19 +0900

    feat: 장애물 자동 spawn system 추가
    - 3종 장애물 prefab 자동 생성 후 spawn
    - _spawnInterval후 장애물 spawn
    - 장애물이 왼쪽 경계에 도달할 시 자동으로 ObjectPool로 반환

--- Assets/Scripts/Obstacle/Obstacle.cs
+++ Assets/Scripts/Obstacle/Obstacle.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour, IPool
@@ -5,6 +6,29 @@ public class Obstacle : MonoBehaviour, IPool
     [SerializeField] private string _name;
     
     public string Name => _name;
+
+    private float _leftLimit; // 왼쪽 경계
+    
+    private void Start()
+    {
+        // 왼쪽 경계 설정
+        var main = Camera.main;
+        if (main != null)
+        {
+            float xScreenHalfSize = main.orthographicSize * main.aspect;
+
+            _leftLimit = -xScreenHalfSize * 2;
+        }
+    }
+
+    private void Update()
+    {
+        // 왼쪽 경계에 도달하면 오브젝트 풀로 반환
+        if (transform.position.x < _leftLimit)
+        {
+            ObjectPoolManager.Instance.ReturnObject(this);
+        }
+    }
 
     public void GetFromPool()
     {
